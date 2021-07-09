fc6e1dcf65cde09f3d614039f37114f6f7791f10
Pranjal Walia <pranjalwalia77@gmail.com>
2021-07-09 11:24:23 +0530

    feat: update changes

--- services/profile/functions/create-user.ts
+++ services/profile/functions/create-user.ts
@@ -2,10 +2,9 @@ import { IResponse } from '../../../common/interfaces/IResponse';
 import { error, response } from '../../../common/utils/http-response';
 import { ICreateUserProfileBody } from '../interfaces';
 import {
-  createNewUserAndStoreToDb
-  // fetchUserByEmail,
-  // fetchUserByMobileNumber,
-  // fetchUserByUsername
+  createNewUserAndStoreToDb,
+  fetchUserByEmail,
+  fetchUserByMobileNumber
 } from '../libs/database-access';
 import { createUserAccountSchemaValidator as validateRequestBodySchema } from '../libs/schema-validator';
 
@@ -21,6 +20,31 @@ const createUser = async (body: ICreateUserProfileBody): Promise<IResponse> => {
   const requestBodyIsValid = await validateRequestBodySchema(body);
   if (requestBodyIsValid !== true) {
     return requestBodyIsValid;
+  }
+
+  const accountWithMobileNumber = await fetchUserByMobileNumber(
+    `+${body.countryCode}${body.mobileNumber}`
+  );
+  const accountWithEmail = await fetchUserByEmail(body.email);
+
+  if (accountWithEmail === false || accountWithMobileNumber === false) {
+    return error(
+      500,
+      30512,
+      'Database error while trying to check if user already exists with the given "email", "mobileNumber"'
+    );
+  }
+
+  if (accountWithEmail !== null) {
+    return error(400, 30424, 'Given email is connected to another account.');
+  }
+
+  if (accountWithMobileNumber !== null) {
+    return error(
+      400,
+      30408,
+      'Given mobile number is connected to another account.'
+    );
   }
 
   const newUserCreated = await createNewUserAndStoreToDb(body);
