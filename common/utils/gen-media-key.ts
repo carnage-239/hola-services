563330084bd888087d5326a2d3c31aaee7a09418
Pranjal Walia <pranjalwalia77@gmail.com>
2021-07-11 13:13:28 +0530

    feat: deps restructure

--- common/utils/gen-media-key.ts
+++ common/utils/gen-media-key.ts
@@ -1,10 +1,10 @@
-import { customAlphabet } from 'nanoid';
+// import { customAlphabet } from 'nanoid';
 
-const VALID_CHARS = '0123456789abcdefghijklmnopqrstuvwxyz';
-const SIZE = 30;
+// const VALID_CHARS = '0123456789abcdefghijklmnopqrstuvwxyz';
+// const SIZE = 30;
 
-export const generateMediaKey = (): string => {
-  const generator = customAlphabet(VALID_CHARS, SIZE);
-  const key = generator();
-  return key;
-};
+// export const generateMediaKey = (): string => {
+//   const generator = customAlphabet(VALID_CHARS, SIZE);
+//   const key = generator();
+//   return key;
+// };
